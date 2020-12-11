fe25d55e6b64c0f4693c8fccddd673e0b35e5eb6
Parker Jones <parker.harold.jones@gmail.com>
2020-12-11 10:27:48 -0700

    Made working random computer choice code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 let UC = document.getElementById("UsersChoice")
 let compChoice = document.getElementById("ComputerChoice")
+let result = document.getElementById("Result")
+
 
 let rock = ""
 let paper = ""
@@ -17,6 +19,39 @@ handleClick = (choice) => {
         UC.innerHTML = "Scissors"
     }
 }
+
+
+
+computerClick = (randomChoice) => {
+randomChoice = ["Rock", "Paper", "Scissors"]; 
+
+randomChoice = randomChoice[Math.floor(Math.random()*randomChoice.length)];
+compChoice.innerHTML = randomChoice; 
+}
+
+
+results = () => {
+if (compChoice.innerHTML = UC.innerHTML) {
+    result.innerHTML = "It is a tie";
+} else if (compChoice.innerHTML < UC.innerHTML){
+    result.innerHTML = "You won!"
+} else {
+    result.innerHTML = "The Computer won"
+}
+public static void results 
+}
+
+
+
+
+
+// computerClick = (computer) => {
+//     options = ["Rock", "Paper", "Scissors"]
+//     compChoice.inn
+// } 
+
+
+ 
 
 
 // computerClick = () => {
